422e63977c976e4441cf7386d1102ab8def62fa8
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2022-07-01 13:15:49 -0700

    pay

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -3,16 +3,18 @@ import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
-import { pay, removeFromCard } from "../store/actions/cardActions.js";
+import { removeFromCard } from "../store/actions/cardActions.js";
 import { toast } from 'react-toastify';
 import CurrencyExchangeOutlinedIcon from '@mui/icons-material/CurrencyExchangeOutlined';
+import { pay } from '../store/actions/payActions.js';
 
 
 
 
 const CardItem = ({video}) => {
+
 
 
   const navigate = useNavigate();
@@ -24,6 +26,9 @@ const CardItem = ({video}) => {
   const userSigninState = useSelector( state => state.userSigninReducer );
   const { userInfo } = userSigninState;
   
+  const payState = useSelector( state => state.payReducer );
+  const { error, loading, payment } = payState;
+  console.log(payment)
   
   
   const dispatch = useDispatch();
@@ -46,7 +51,6 @@ const CardItem = ({video}) => {
       dispatch(pay(price, id));
     }
 
-
     return (
       <>
     <div className='p-1 w-72 h-80 bg-white flex flex-col rounded-xl shadow-sm shadow-dark sm:hover:scale-105  duration-150 font-firstFont'>
@@ -66,7 +70,7 @@ const CardItem = ({video}) => {
           { (cardItems && cardItems.find( cardItem => cardItem.id === id )) ?  <button className=' h-full w-full rounded-md shadow-sm shadow-dark font-bold text-dark flex items-center justify-center gap-2' onClick={()=>removeFromCartHandler(cardItems.find( cardItem => cardItem.id === id ).id)}><DeleteOutlineOutlinedIcon/><span>حذف از سبد خرید</span></button> : <button className='bg-orange h-full w-full rounded-md shadow-sm shadow-dark font-bold text-dark flex items-center justify-center gap-2' onClick={addToCardHandler}><AddShoppingCartIcon/><span>افزودن به سبد خرید</span> </button> }
           </div>
           <div className="basis-1/6  p-1 text-xs">
-          <button className='bg-dark text-theWhite font-thin h-full w-full rounded-md shadow-sm shadow-dark  flex items-center justify-center gap-3' onClick={payHandler} ><CurrencyExchangeOutlinedIcon className='font-thin'/><span>پرداخت</span></button>
+          { payment ? <button><a  href={`https://www.zarinpal.com/pg/StartPay/${payment}`}> برو</a></button> :<button className='bg-dark text-theWhite font-thin h-full w-full rounded-md shadow-sm shadow-dark  flex items-center justify-center gap-3' onClick={payHandler}>{ loading? <CurrencyExchangeOutlinedIcon className='font-thin animate-spin'/> :<CurrencyExchangeOutlinedIcon className='font-thin'/>} {loading ? <span>آغاز فرایند پزداخت</span> : <span>پرداخت</span>}</button>}
           </div>
           <div className="basis-1/6  p-1 text-xs">
           <button disabled className='bg-dark text-theWhite font-thin h-full w-full rounded-md shadow-sm shadow-dark  flex items-center justify-center gap-2' ><FileDownloadOutlinedIcon className='font-thin'/><span>دریافت لینک دانلود</span></button>
