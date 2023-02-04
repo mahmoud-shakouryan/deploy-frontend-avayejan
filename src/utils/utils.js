6e0b561d9356cdb44480cd9882498e91c7575cd6
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2023-02-04 23:17:33 +0330

    commit 15bahman

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,19 @@
-export const enToPerNum = (enNum) => { 
-    console.log(enNum)
+export const enToPerNum = (enNum) => {
     const enNumStr = enNum.toString();
-    const perDigits = '۰۱۲۳۴۵۶۷۸۹'.split('');
-    const per_num = enNumStr.replace(/\d/g, m => perDigits[parseInt(m)]);
+    const perDigits = "۰۱۲۳۴۵۶۷۸۹".split("");
+    const per_num = enNumStr.replace(/\d/g, (m) => perDigits[parseInt(m)]);
     return per_num;
- };
-
- export const getHahtagForm = (category) =>{
-    const underlined = category.replace(' ', '_');
-    return underlined + '#';
-}
+  };
+  
+  export const getHahtagForm = (category) => {
+    const underlined = category.replaceAll(" ", "_");
+    return underlined + "#";
+  };
+  
+  export const getExpalantoryDuration = (duration) => {
+    const durationTimeSplit = duration.split(":").map((a) => +a);
+    const hours = durationTimeSplit[0];
+    const minutes = durationTimeSplit[1];
+    return [hours, minutes];
+  };
+  
