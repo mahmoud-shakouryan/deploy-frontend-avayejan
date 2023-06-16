e4b45158952608fcc0e395149aa314bcca9ee2b5
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2023-06-16 20:58:34 +0430

    commit

--- src/store/actions/videoActions.js
+++ src/store/actions/videoActions.js
@@ -8,7 +8,10 @@ export const videoList = (page) => {
     try {
       const vidsToSkip = page === 0 || page === 1 ? 0 : (page - 1) * 6;
       const thisPageVids = vidsArr.slice(vidsToSkip, vidsToSkip + 6);
-      dispatch({ type: actions.VIDEO_LIST_SUCCESS, payload: { videos: vidsArr, thisPageVids: thisPageVids }});
+      dispatch({
+        type: actions.VIDEO_LIST_SUCCESS,
+        payload: { videos: vidsArr, thisPageVids: thisPageVids },
+      });
     } catch (err) {
       console.log("videoAcions videoList error", err.message);
       dispatch({ type: actions.VIDEO_LIST_FAIL, payload: err.message });
@@ -22,41 +25,47 @@ export const videoDetails = (id) => {
       dispatch({ type: actions.VIDEO_DETAILS_REQUEST });
       const video = vidsArr.find((video) => video.id === id);
       if (!video) {
-        return dispatch({type: actions.VIDEO_DETAILS_FAIL, payload: "ویدیو یافت نشد"})
+        return dispatch({
+          type: actions.VIDEO_DETAILS_FAIL,
+          payload: "ویدیو یافت نشد",
+        });
       }
       dispatch({ type: actions.VIDEO_DETAILS_SUCCESS, payload: video });
     } catch (err) {
-      console.log("videoAcions videoDetails error", err.message) 
-      dispatch({ type: actions.VIDEO_DETAILS_FAIL, payload: err.message })}
+      console.log("videoAcions videoDetails error", err.message);
+      dispatch({ type: actions.VIDEO_DETAILS_FAIL, payload: err.message });
+    }
   };
 };
 
-
 export const myVidsAction = (status, userId, payId, order_id) => {
-  return async dispatch =>{
-        dispatch({ type: actions.MY_VIDS_REQUEST});
-        try{
-          const { data } = await axios.post('https://www.avayejaan.ir/api/videos/uservids', {userId, payId, order_id, status});
-          dispatch({ type: actions.MY_VIDS_SUCCESS, payload: data })
-        }
-        catch(err){
-          dispatch({ type: actions.MY_VIDS_FAIL, payload: err.message });
-        }
-  }
-}
+  return async (dispatch) => {
+    dispatch({ type: actions.MY_VIDS_REQUEST });
+    try {
+      const { data } = await axios.post(
+        "https://www.avayejaan.ir/api/videos/uservids",
+        { status, userId, payId, order_id }
+      );
+      dispatch({ type: actions.MY_VIDS_SUCCESS, payload: data });
+    } catch (err) {
+      dispatch({ type: actions.MY_VIDS_FAIL, payload: err.message });
+    }
+  };
+};
 
-
-export const myVidsLinksAction = (allFiles) =>{
-   return async dispatch => {
-       dispatch({ type: actions.LINK_REQUEST });
-       try{
-           const { data } = await axios.post('https://www.avayejaan.ir/api/videos/myvidslinks', { allFiles: allFiles });
-           console.log('links action', data)
-           dispatch({ type: actions.LINK_SUCCESS , payload: data });
-       }
-       catch(err){
-           console.log('linksAction axios error >>>', err);
-           dispatch({ type: actions.LINK_FAIL, payload: err.message });
-       }
-   }
+export const myVidsLinksAction = (allFiles) => {
+  return async (dispatch) => {
+    dispatch({ type: actions.LINK_REQUEST });
+    try {
+      const { data } = await axios.post(
+        "https://www.avayejaan.ir/api/videos/myvidslinks",
+        { allFiles: allFiles }
+      );
+      console.log("links action", data);
+      dispatch({ type: actions.LINK_SUCCESS, payload: data });
+    } catch (err) {
+      console.log("linksAction axios error >>>", err);
+      dispatch({ type: actions.LINK_FAIL, payload: err.message });
+    }
+  };
 };
