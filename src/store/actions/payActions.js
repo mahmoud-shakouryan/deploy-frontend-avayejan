e72b116d0e147ce4130a90234803d6aa0dc3dc71
mahmoud <mahmoud.shakouryan.dev@gmail.com>
2023-06-26 23:13:34 +0430

    commit

--- src/store/actions/payActions.js
+++ src/store/actions/payActions.js
@@ -1,39 +1,40 @@
 import * as actions from "../actions/actionTypes";
-import axios from 'axios';
-import { signinAction } from '../actions/userActions';
+import axios from "axios";
+import { signinAction } from "../actions/userActions";
 import { videoList } from "./videoActions";
 
-
-
-export const pay = (price, videoId) =>{
-    return async (dispatch, getState) =>{
-        dispatch({type: actions.PAY_REQUEST});
-        const store = getState();
-        const userInfo = store.userSigninReducer.userInfo;
-        try{
-            const result = await axios.post('https://www.avayejaan.ir/api/pay', { price: price, videoId: videoId, userToken: userInfo.token }, {headers: {Authorization: `Bearer ${userInfo.token}`}});
-            dispatch({ type: actions.PAY_SUCCESS, payload: result.data.link });
-        }
-        catch(err){
-            console.log('pay error>>>', err);
-            dispatch({ type: actions.PAY_FAIL, payload: err.message });
-
-        }
+export const pay = (price, videoId) => {
+  return async (dispatch, getState) => {
+    dispatch({ type: actions.PAY_REQUEST });
+    const store = getState();
+    const userInfo = store.userSigninReducer.userInfo;
+    try {
+      const result = await axios.post(
+        "https://www.avayejaan.ir/api/pay",
+        { price: price, videoId: videoId, userToken: userInfo.token },
+        { headers: { Authorization: `Bearer ${userInfo.token}` } }
+      );
+      dispatch({ type: actions.PAY_SUCCESS, payload: result.data.link });
+    } catch (err) {
+      console.log("pay error>>>", err);
+      dispatch({ type: actions.PAY_FAIL, payload: err.message });
     }
+  };
 };
 
 export const getPaymentStatusAction = (status, order_id, payId) => {
-    return async dispatch =>{
-        dispatch({ type: actions.GET_PAYMENT_STATUS_REQUEST});
-        try{
-            const {data} = await axios.post('https://www.avayejaan.ir/api/pay/status', { status: status, order_id: order_id, payId: payId});
-            dispatch(signinAction(data.mail, data.token));
-            dispatch(videoList(1));
-            dispatch({ type: actions.GET_PAYMENT_STATUS_SUCCESS});    //data>>> user mail && user token
-        }
-        catch(err){
-            console.log('getPaymentStatusAction axios error >>>', err);
-            dispatch({ type: actions.GET_PAYMENT_STATUS_FAIL, payload: err.message })
-        }
+  return async (dispatch) => {
+    dispatch({ type: actions.GET_PAYMENT_STATUS_REQUEST });
+    try {
+      const { data } = await axios.post(
+        "https://www.avayejaan.ir/api/pay/status",
+        { status: status, order_id: order_id, payId: payId }
+      );
+      dispatch(signinAction(data.mail, data.name));
+      dispatch({ type: actions.GET_PAYMENT_STATUS_SUCCESS, payload: data });
+    } catch (err) {
+      console.log("getPaymentStatusAction axios error >>>", err);
+      dispatch({ type: actions.GET_PAYMENT_STATUS_FAIL, payload: err.message });
     }
+  };
 };
--- src/store/reducers/payReducers.js
+++ src/store/reducers/payReducers.js
@@ -1,33 +1,34 @@
-import * as actions from '../actions/actionTypes';
+import * as actions from "../actions/actionTypes";
 
+const payReducerInState = { payment: "", error: false, loading: false };
 
-const payReducerInState = { payment: '', error: false, loading: false };
-
-export const payReducer = (state= payReducerInState, action ) =>{
-    switch (action.type){
-        case actions.PAY_REQUEST:
-            return {...state, loading:true };
-        case actions.PAY_SUCCESS:
-            return { ...state, loading: false, payment: action.payload };
-        case actions.PAY_FAIL:
-            return { ...state, loading:false, error: action.payload };
-        default: 
-            return state;
-    }
+export const payReducer = (state = payReducerInState, action) => {
+  switch (action.type) {
+    case actions.PAY_REQUEST:
+      return { ...state, loading: true };
+    case actions.PAY_SUCCESS:
+      return { ...state, loading: false, payment: action.payload };
+    case actions.PAY_FAIL:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
 };
 
+const paymentStatusInitialState = { loading: false, error: null };
 
-const paymentStatusInitialState = { loading: true, error: null };
-
-export const paymentStatusReducer = (state = paymentStatusInitialState, action) => {
-    switch(action.type){
-        case actions.GET_PAYMENT_STATUS_REQUEST:
-            return { ...state, loading: true, error: false };
-        case actions.GET_PAYMENT_STATUS_SUCCESS:
-            return { ...state, loading: false, error: false };
-        case actions.GET_PAYMENT_STATUS_FAIL:
-            return { ...state, loading: false, error: action.payload };
-        default:
-         return state;
-    }
+export const paymentStatusReducer = (
+  state = paymentStatusInitialState,
+  action
+) => {
+  switch (action.type) {
+    case actions.GET_PAYMENT_STATUS_REQUEST:
+      return { ...state, loading: true, error: false };
+    case actions.GET_PAYMENT_STATUS_SUCCESS:
+      return { ...state, loading: false, error: false };
+    case actions.GET_PAYMENT_STATUS_FAIL:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
 };
